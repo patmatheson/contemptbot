eb005d3ba6139242735faba108938da05f04a7a2
Pat <pat.matheson@gmail.com>
2021-11-22 20:46:59 -0600

    Added contempt, added check for Nickname

---
+++ commands/contempt.js
@@ -0,0 +1,17 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('contempt')
+		.setDescription('Hate Hate hate hate Hate!')
+        .addUserOption(option => option.setName('target').setDescription('Select a user').setRequired(true)),
+        
+	async execute(interaction) {
+        //await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
+        //await interaction.reply(`Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`);
+        const member = interaction.options.getMember('target');
+        const name = member.nickname ?? member.user.username;
+        
+        await interaction.reply(`I hate you: ${name}!`);
+	},
+};
