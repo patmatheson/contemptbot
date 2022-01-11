3d5977abd5d7078ced2c4e82b066e44eeb2668b4
Pat <pat.matheson@gmail.com>
2022-01-10 19:07:55 -0600

    changed way commands are sent to manual https POST

--- deploy-commands.js
+++ deploy-commands.js
@@ -2,17 +2,28 @@ const fs = require('fs');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
 const { clientId, guildId, token } = require('./config.json');
+const { SlashCommandBuilder } = require('@discordjs/builders');
 
+
+
+/*
+// Dumb Way of "Automatically" adding commands
 const commands = [];
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
 	commands.push(command.data.toJSON());
-}
+}*/
+
+const commands =[
+	new SlashCommandBuilder()
+]
+
 
 const rest = new REST({ version: '9' }).setToken(token);
 
-rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+rest.put(Routes.applicationCommands(clientId), 
+		{ body: commands })
 	.then(() => console.log('Successfully registered application commands.'))
 	.catch(console.error);
---
+++ register-commands.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+const https = require('https')
+const { clientId, guildId, token } = require('./config.json');
+
+let hostname = `discord.com`;
+let path = `/api/v8/applications/${clientId}/commands`;
+
+console.log(`${ hostname + path }`);
+
+let jsonData = fs.readFileSync('./commandjson/contempt.json');
+let commands = JSON.parse(jsonData);
+//console.log(commands);
+
+const data = JSON.stringify(commands);
+
+const options ={
+    hostname: hostname,
+    port: 443,
+    path: path,
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+        'Content-Length': data.length,
+        'Authorization': `Bot ${token}`
+    }
+}
+
+const req = https.request(options, res => {
+    console.log(`statusCode: ${res.statusCode}`)
+  
+    res.on('data', d => {
+      console.log(d)
+    })
+  })
+
+req.on('error', error => {
+    console.error(error)
+})
+
+req.write(data)
+req.end()
