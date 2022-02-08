66cd509e3a84cde301be8622c7208fcc6a711138
Pat <pat.matheson@gmail.com>
2022-02-07 22:46:10 -0600

    fixing http keep alive

--- index.ts
+++ index.ts
@@ -10,7 +10,7 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Iam m still alive\n');
-}).listen(80, "x.x.x.x");
+}).listen(80, "0.0.0.0");
 console.log('Server listening to port 80.');
 
 
