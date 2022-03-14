123555c8d4bea3c10776db4e04efefa3d2201bcb
Pat <pat.matheson@gmail.com>
2022-03-14 17:59:15 -0600

    removed superflous commands, fixed contempt command data, added regContempt command

---
+++ commands/regcontempt.ts
@@ -0,0 +1,42 @@
+import { SlashCommandBuilder } from "@discordjs/builders";
+import { REST } from "@discordjs/rest";
+import { Routes } from 'discord-api-types/v9';
+import { token, clientId } from '../config.json';
+import * as fs from 'fs';
+
+
+
+const regContemptCommmand = {
+    data: new SlashCommandBuilder()
+        .setName('regcontempt')
+        .setDescription('Register Commands for ContemptBot on this Server'),
+
+    async execute (interaction, client){
+        const commandFiles = fs
+            .readdirSync('./commands')
+            .filter(file => file.endsWith('.js'));
+        
+        const commands = [];
+        
+        for (const file of commandFiles){
+            const guildCommands = require(`./${file}`);
+            commands.push(guildCommands.data.toJSON());
+        }
+
+        const guildId = interaction.guild.id;
+
+        const rest = new REST({version: '9'}).setToken(token);
+        try{
+            await rest.put(
+                Routes.applicationGuildCommands(clientId, guildId),
+                { body: commands },
+            )
+        } catch (error){
+            console.error(error);
+        }
+    }
+}
+
+export {
+    regContemptCommmand,
+}
